2792bca9a586f59735733cab0e4722174ce21cb3
Monoget <monoget2@gmail.com>
2023-08-19 16:08:50 +0600

    update all

--- search.php
+++ search.php
@@ -7,6 +7,35 @@ if (!isset($_SESSION['userid'])) {
     header("Location: login.php");
 }
 $userId = $_SESSION['userid'];
+
+if (isset($_POST['apply'])) {
+    $job_id = $_POST['job_id'];
+    $inserted_at = date('Y-m-d h:i:s');
+
+
+    $query = "INSERT INTO `job_apply`(`job_id`, `customer_id`, `inserted_at`) VALUES ('$job_id','$userId','$inserted_at')";
+    $data = $db_handle->insertQuery($query);
+
+
+    echo "<script>
+                alert('Job Applied');
+                window.location.href='search.php';
+                </script>";
+
+}
+
+if (isset($_POST['favourite'])) {
+    $job_id = $_POST['job_id'];
+    $inserted_at = date('Y-m-d h:i:s');
+
+    $query = "INSERT INTO `favorite`( `customer_id`, `job_id`, `inserted_at`) VALUES ('$userId','$job_id','$inserted_at')";
+    $data = $db_handle->insertQuery($query);
+    echo "<script>
+                alert('Favourite Changed');
+                window.location.href='search.php';
+                </script>";
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -122,58 +151,132 @@ $userId = $_SESSION['userid'];
             </div>
 
             <?php
-            $query_add_on='';
-            if(isset($_GET['job_type'])){
-                $query_add_on=" and j.job_type='{$_GET['job_type']}'";
+            $query_add_on = '';
+            if (isset($_GET['job_type'])) {
+                $query_add_on = " and j.job_type='{$_GET['job_type']}'";
             }
 
-            $query = "SELECT * FROM company as c,job_post as j where c.id=j.company_id".$query_add_on." order by j.id desc";
+            $query = "SELECT * FROM company as c,job_post as j where c.id=j.company_id" . $query_add_on . " order by j.id desc";
             $data = $db_handle->runQuery($query);
             $row_count = $db_handle->numRows($query);
             for ($i = 0; $i < $row_count; $i++) {
-                ?>
 
-                <div class="row ms-1 me-1">
-                    <div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-4 p-2 d-flex justify-content-center align-items-center"
-                                 style="background: #383b40;">
-                                <div>
-                                    <img alt="..." class="img-fluid" src="<?php echo $data[$i]["image"]; ?>"
-                                         style="border: 3px solid white;border-radius: 15px;">
-                                    <p class="text-white text-center mt-2"
-                                       style="font-family: 'DMSans-Bold', serif;"><?php echo $data[$i]["name"]; ?></p>
-                                </div>
-                            </div>
-                            <div class="col-8">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo $data[$i]["job_title"]; ?></h5>
-                                    <p class="card-text"><small class="text-muted"><img alt="Flag of Country"
-                                                                                        src="assets/images/12/hk.webp"
-                                                                                        style="width: 15px;"> <?php echo $data[$i]["address"]; ?>
-                                        </small>
-                                    </p>
-                                    <p class="card-text"><span
-                                                class="price">$<?php echo $data[$i]["salary"]; ?> HKD</span><small
-                                                class="text-muted"> per
-                                            hour</small></p>
-                                    <p class="card-text"><small
-                                                class="text-muted"><?php echo $data[$i]["keywords"]; ?></small></p>
-                                </div>
-                                <div class="row">
-                                    <div class="col-4">
-                                        <button class="btn price-button" type="button" data-bs-toggle="modal"
-                                                data-bs-target="#staticBackdrop">Detail
-                                        </button>
+                $apply_job = $db_handle->numRows("select * from job_apply where job_id={$data[$i]['id']}");
+
+                if ($apply_job == 0) {
+                    ?>
+
+                    <div class="row ms-1 me-1">
+                        <div class="card mb-3" style="max-width: 540px;">
+                            <div class="row g-0">
+                                <div class="col-4 p-2 d-flex justify-content-center align-items-center"
+                                     style="background: #383b40;">
+                                    <div>
+                                        <img alt="..." class="img-fluid" src="<?php echo $data[$i]["image"]; ?>"
+                                             style="border: 3px solid white;border-radius: 15px;">
+                                        <p class="text-white text-center mt-2"
+                                           style="font-family: 'DMSans-Bold', serif;"><?php echo $data[$i]["name"]; ?></p>
                                     </div>
                                 </div>
-                            </div>
+                                <div class="col-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $data[$i]["job_title"]; ?></h5>
+                                        <p class="card-text"><small class="text-muted"><img alt="Flag of Country"
+                                                                                            src="assets/images/12/hk.webp"
+                                                                                            style="width: 15px;"> <?php echo $data[$i]["address"]; ?>
+                                            </small>
+                                        </p>
+                                        <p class="card-text"><span
+                                                    class="price">$<?php echo $data[$i]["salary"]; ?> HKD</span><small
+                                                    class="text-muted"> per
+                                                hour</small></p>
+                                        <p class="card-text"><small
+                                                    class="text-muted"><?php echo $data[$i]["keywords"]; ?></small></p>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <button class="btn price-button" type="button" data-bs-toggle="modal"
+                                                    data-bs-target="#staticBackdrop<?php echo $data[$i]["id"]; ?>">
+                                                Detail
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-
-            <?php } ?>
+                    <div class="modal fade" id="staticBackdrop<?php echo $data[$i]["id"]; ?>" data-bs-backdrop="static"
+                         data-bs-keyboard="false" tabindex="-1"
+                         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header justify-content-center align-items-center">
+                                    <a class="text-decoration-none text-white card-checkbox" data-bs-dismiss="modal"
+                                       aria-label="Close"><i class="fa-solid fa-chevron-left"></i></a>
+                                    <?php
+                                    $favourite = $db_handle->numRows("select * from favorite where job_id={$data[$i]['id']}");
+                                    ?>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="job_id"
+                                               value="<?php echo $data[$i]["id"]; ?>" required>
+                                        <button type="submit" name="favourite" class="card-heart"><i
+                                                    class="fas fa-heart <?php if ($favourite % 2 == 1) echo 'text-danger'; ?>"></i>
+                                        </button>
+                                    </form>
+                                    <div class="row">
+                                        <div class="col-12 text-center">
+                                            <img src="<?php echo $data[$i]["image"]; ?>" class="img-fluid" alt=""
+                                                 style="border: 3px solid white;border-radius: 15px;"/>
+                                        </div>
+                                        <div class="col-12">
+                                            <h1 class="text-center text-white mt-3"
+                                                style="font-family: 'DMSans-Bold', serif;"><?php echo $data[$i]["name"]; ?></h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-body">
+                                    <h2 class="text-center"><?php echo $data[$i]["job_title"]; ?></h2>
+                                    <p>
+                                        <?php echo $data[$i]["job_description"]; ?>
+                                    </p>
+                                    <div>
+                                        <p>
+                                            <span class="badge text-bg-success">Salary/Rate:</span>
+                                            $<?php echo $data[$i]["salary"]; ?> HKD per hour
+                                        </p>
+                                        <p>
+                                            <span class="badge text-bg-success">Job type:</span> <?php echo $data[$i]["job_type"]; ?>
+                                        </p>
+                                        <p>
+                                            <span class="badge text-bg-success">Address:</span> <?php echo $data[$i]["address"]; ?>
+                                        </p>
+                                        <p>
+                                            <span class="badge text-bg-success">Contact:</span> <?php echo $data[$i]["contact"]; ?>
+                                        </p>
+                                        <p>
+                                            <span class="badge text-bg-success">Rating:</span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="modal-footer" style="border-top: unset">
+                                    <form action="" method="post">
+                                        <input type="hidden" name="job_id" value="<?php echo $data[$i]["id"]; ?>"
+                                               required>
+                                        <button type="submit" class="btn btn-success fs-button-apply" name="apply">Apply
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php }
+            } ?>
 
             <div class="row fixed-bottom text-center fs-nav-fixed-bottom">
                 <div class="col-2">
@@ -203,59 +306,6 @@ $userId = $_SESSION['userid'];
                         <i class="fa-solid fa-user"></i>
                     </a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-     aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header justify-content-center align-items-center">
-                <a href="#" class="text-decoration-none text-white card-checkbox" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-chevron-left"></i></a>
-                <button class="card-heart"><i class="fas fa-heart"></i></button>
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <img src="assets/images/company/38426_ad.png" class="img-fluid" alt=""
-                             style="border: 3px solid white;border-radius: 15px;"/>
-                    </div>
-                    <div class="col-12">
-                        <h1 class="text-center text-white mt-3" style="font-family: 'DMSans-Bold', serif;">Magnus</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-body">
-                <h2 class="text-center">Project Manager</h2>
-                <p>
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-                    essentially unchanged.
-                </p>
-                <div>
-                    <p>
-                        <span class="badge text-bg-success">Salary/Rate:</span> $95 HKD per hour
-                    </p>
-                    <p>
-                        <span class="badge text-bg-success">Job type:</span> Full-Time/Part-Time
-                    </p>
-                    <p>
-                        <span class="badge text-bg-success">Address:</span> Central, Hong Kong
-                    </p>
-                    <p>
-                        <span class="badge text-bg-success">Contact:</span> +852 **** ****
-                    </p>
-                    <p>
-                        <span class="badge text-bg-success">Rating:</span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star checked"></span>
-                        <span class="fa fa-star"></span>
-                        <span class="fa fa-star"></span>
-                    </p>
-                </div>
-            </div>
-            <div class="modal-footer" style="border-top: unset">
-                <button type="button" class="btn btn-success fs-button-apply">Apply</button>
             </div>
         </div>
     </div>
